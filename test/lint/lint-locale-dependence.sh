f0aa119e2b3561ae338551a46f2abf8a5c5630db
cryptonecoin <wowgamer13377@hotmail.com>
2019-10-08 19:46:52 +0200

    test/
    test/ updated

--- test/lint/lint-locale-dependence.sh
+++ test/lint/lint-locale-dependence.sh
@@ -2,8 +2,8 @@
 
 export LC_ALL=C
 KNOWN_VIOLATIONS=(
-    "src/bitcoin-tx.cpp.*stoul"
-    "src/bitcoin-tx.cpp.*trim_right"
+    "src/cryptonecoin-tx.cpp.*stoul"
+    "src/cryptonecoin-tx.cpp.*trim_right"
     "src/dbwrapper.cpp.*stoul"
     "src/dbwrapper.cpp:.*vsnprintf"
     "src/httprpc.cpp.*trim"
